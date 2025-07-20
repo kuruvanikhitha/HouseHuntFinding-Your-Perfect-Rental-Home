df38af4dd02a797b5c02c44704082a6d43250507
GeethaPranathi <pranathikavyakanala@gmail.com>
2025-07-20 11:38:41 +0530

    Delete backend directory

--- backend/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authenticateUser = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/protected', authenticateUser, (req, res) => {
-  res.json({ message: `Welcome ${req.user.name}, this is protected data.` });
-});
-
-module.exports = router;
